754326963e3ba53e7ddd9283fcbc3e43a5367d38
Thanathip Gorlph <gorlph@hotmail.com>
2021-07-10 16:26:03 +0700

    commit nodejs binding boilerplate

---
+++ nodejs_binding/src/lib.rs
@@ -0,0 +1,16 @@
+use neon::prelude::*;
+ีuse oxidized_thainlp_main_lib as tokenizer;
+
+
+
+
+
+fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
+    Ok(cx.string("hello node"))
+}
+
+#[neon::main]
+fn main(mut cx: ModuleContext) -> NeonResult<()> {
+    cx.export_function("hello", hello)?;
+    Ok(())
+}
