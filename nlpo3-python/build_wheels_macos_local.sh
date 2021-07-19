441fd0f0e516bcaa84900de3c9ff9e3f350b6fcb
Arthit Suriyawongkul <arthit@gmail.com>
2021-07-19 11:26:17 +0100

    Test building on all platforms [cd build]

---
+++ nlpo3-python/build_wheels_macos_local.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# The main build wheel workflow is on GitHub,
+# see .github/wheels.yml at the root of main repo.
+# This script is meant to be run local and use for testing purpose only.
+
+# Use pyenv to build on different Python versions.
+
+# If "error: implicit declaration of function 'sendfile' is invalid in C99"
+# occurs when installing a Python version, see fix at:
+# https://github.com/pyenv/pyenv/issues/1740#issuecomment-738749988
+
+set -ex
+
+# store pyenv Python version before calling the script
+SAVE_PYVER=$(pyenv global)
+
+for PYVER in $(ls ~/.pyenv/versions); do
+    PYVER_MINOR=$(echo "${PYVER}" | sed -nre 's/^(([0-9]+\.)?[0-9]+).*/\1/p')
+    echo "Build for Python ${PYVER_MINOR}"
+    pyenv global "${PYVER}"
+    $(pyenv which pip) install -U pip
+    $(pyenv which pip) install -U build setuptools setuptools-rust wheel
+    $(pyenv which python) -m build --wheel
+done
+
+# restore pyenv Python version
+pyenv global ${SAVE_PYVER}
---
+++ nlpo3-python/build_wheels_manylinux_local.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# The main build wheel workflow is on GitHub,
+# see .github/wheels.yml at the root of main repo.
+# This script is meant to be run local and use for testing purpose only.
+
+# This script has to run through manylinux docker image:
+# docker run --rm -v `pwd`:/io quay.io/pypa/manylinux2014_x86_64 bash /io/build_wheels_manylinux_local.sh
+
+set -ex
+
+curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain stable -y
+export PATH="$HOME/.cargo/bin:$PATH"
+
+cd /io
+
+for PYBIN in /opt/python/cp{36,37,38,39}*/bin; do
+    "${PYBIN}/pip" install -U build setuptools setuptools-rust wheel
+    "${PYBIN}/python" -m build --wheel
+done
+
+for whl in dist/*linux*.whl; do
+    auditwheel repair "$whl" -w dist/
+done
