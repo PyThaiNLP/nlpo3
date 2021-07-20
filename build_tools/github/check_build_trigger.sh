8eed2f741b423b9e559671f5ae30ab0445cd0987
Arthit Suriyawongkul <arthit@gmail.com>
2021-07-21 00:53:54 +0100

    $GITHUB_EVENT_NAME == workflow_dispatch will also trigger build wheel [cd build]

--- build_tools/github/check_build_trigger.sh
+++ build_tools/github/check_build_trigger.sh
@@ -6,6 +6,7 @@ set -x
 COMMIT_MSG=$(git log --no-merges -1 --oneline)
 
 # The commit marker "[cd build]" will trigger the build when required
-if [[ "$COMMIT_MSG" =~ "[cd build]" ]]; then
+if [[ "$GITHUB_EVENT_NAME" == "workflow_dispatch" ||
+      "$COMMIT_MSG" =~ "[cd build]" ]]; then
     echo "::set-output name=build::true"
 fi
