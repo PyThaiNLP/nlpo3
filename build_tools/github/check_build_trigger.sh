707f7d7f3c9cce51569e466e7098a52a7f346add
Arthit Suriyawongkul <arthit@gmail.com>
2021-07-21 01:53:32 +0100

    Add on: release

--- build_tools/github/check_build_trigger.sh
+++ build_tools/github/check_build_trigger.sh
@@ -7,6 +7,7 @@ COMMIT_MSG=$(git log --no-merges -1 --oneline)
 
 # The commit marker "[cd build]" will trigger the build when required
 if [[ "$GITHUB_EVENT_NAME" == "workflow_dispatch" ||
+      "$GITHUB_EVENT_NAME" == "release" ||
       "$COMMIT_MSG" =~ "[cd build]" ]]; then
     echo "::set-output name=build::true"
 fi
