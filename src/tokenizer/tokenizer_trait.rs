d830b4580071333951f99ecd0884e050b1b50d06
Thanathip Gorlph <gorlph@hotmail.com>
2021-08-07 15:03:27 +0700

    change visibility of many modules

--- src/tokenizer/tokenizer_trait.rs
+++ src/tokenizer/tokenizer_trait.rs
@@ -1,11 +1,11 @@
-/**
-   This should be the only part exposed to lib.rs
-*/
 use anyhow::Result as AnyResult;
 pub trait Tokenizer {
-    // I need help naming these two functions...
-    
-    fn segment(&self, text: &str, safe: Option<bool>, parallel: Option<bool>) -> AnyResult<Vec<String>>;
+    fn segment(
+        &self,
+        text: &str,
+        safe: Option<bool>,
+        parallel: Option<bool>,
+    ) -> AnyResult<Vec<String>>;
 
     fn segment_to_string(
         &self,
