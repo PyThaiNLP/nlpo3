c968b6b417ce9237d25bffe31b8baf1f9e0430b8
Arthit Suriyawongkul <arthit@gmail.com>
2021-11-09 04:42:07 +0000

    [cd build] Exclude notebooks and tests in package

--- nlpo3-python/setup.py
+++ nlpo3-python/setup.py
@@ -1,7 +1,8 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 from setuptools_rust import Binding, RustExtension, Strip
 
 setup(
+    packages=find_packages(exclude=["notebooks", "tests"]),
     rust_extensions=[
         RustExtension(
             "nlpo3._nlpo3_python_backend",
