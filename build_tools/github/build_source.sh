5079e587766c7eea0bea11b18a6ea11b5da2966c
Arthit Suriyawongkul <arthit@gmail.com>
2026-01-31 17:31:52 +0000

    [cd build]

--- build_tools/github/build_source.sh
+++ build_tools/github/build_source.sh
@@ -12,7 +12,6 @@ python -m pip install --upgrade pip
 python -m pip install --upgrade build setuptools-rust twine
 
 cd nlpo3/nlpo3-python
-# Build sdist using PEP 517 backend via `python -m build`
 python -m build --sdist
 
 twine check dist/*.tar.gz
