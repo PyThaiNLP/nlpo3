a9611418a0106a48740c495becfb142bb7859952
Arthit Suriyawongkul <arthit@gmail.com>
2026-01-31 17:30:23 +0000

    Update MACOSX_DEPLOYMENT_TARGET=10.12

--- build_tools/github/build_source.sh
+++ build_tools/github/build_source.sh
@@ -9,9 +9,10 @@ python -m venv build_env
 source build_env/bin/activate
 
 python -m pip install --upgrade pip
-python -m pip install --upgrade setuptools-rust twine
+python -m pip install --upgrade build setuptools-rust twine
 
 cd nlpo3/nlpo3-python
-python setup.py sdist
+# Build sdist using PEP 517 backend via `python -m build`
+python -m build --sdist
 
 twine check dist/*.tar.gz
