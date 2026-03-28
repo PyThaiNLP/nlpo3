2d94115f5af5a0db23a42c98ecaa620bf186888a
phoneee <phoneee@gmail.com>
2026-03-28 17:32:35 +0700

    Add lk82 soundex

--- src/soundex/mod.rs
+++ src/soundex/mod.rs
@@ -1,7 +1,8 @@
 // SPDX-FileCopyrightText: 2024 PyThaiNLP Project
 // SPDX-License-Identifier: Apache-2.0
 
-mod four_bytes_str;
-pub mod soundex;
-pub mod thai_chars;
-pub mod tokenizer;
+//! Thai soundex algorithms for phonetic matching.
+
+pub mod lk82;
+
+pub use lk82::lk82;
